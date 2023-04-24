9dadf1b6f1b55e3077a37c4fe75a8d73c38295cc
jokatty <jyotikattani@gmail.com>
2023-04-24 14:02:44 +0200

    Add new panda commands

--- tools/pandas_learning.py
+++ tools/pandas_learning.py
@@ -30,3 +30,46 @@ clean_df
 
 # Print a column
 clean_df['Starting Median salary']
+
+# Find the maximum value of a column
+clean_df['Starting Median Salary'].max()
+
+# Find the index / row where the maximum value occures
+clean_df['Starting Median Salary'].idxmax()
+
+# Access other properties(column name) for this row where max value occured, now that we know the row number.
+clean_df['Undergraduate Major'].loc[43]
+
+# or other way of accessing the same thing as above
+clean_df['Undergraduate Major'][43]
+
+# Print the entire row where max value ocuured
+clean_df.loc[43]
+
+# Find the differnce between two columns
+spread = clean_df['Mid-Career 90th Percentile Salary'].subtract(clean_df['Mid-Career 10th Percentile Salary'])
+
+# Insert a column in position "1" in the the data. i.e the second column and 'name' it.
+clean_df.insert(1, 'Spread', spread)
+
+"""Sort the data by a column name"""
+
+low_risk = clean_df.sort_values('Spread')
+
+"""Display the sorted data based on desired column"""
+
+low_risk[['Undergraduate Major', 'Spread']].head()
+
+""""groupby" for knowing different types of catagories"""
+
+clean_df.groupby('Group').count()
+
+"""Find mean by a group (category)"""
+
+clean_df.groupby('Group').mean()
+
+# Tell panda to Formate number in a specific way
+pd.options.display.float_format = '{:,.2f}'.format
+
+# now the numbers will be printed in the specified format(, after 3 digits and 2 places after decimal)
+clean_df.groupby('Group').mean()
